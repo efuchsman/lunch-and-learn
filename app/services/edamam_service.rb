620b67c689cfc37594cb024d8ac2c0f786301c5e
Eli Fuchsman <efuchsman@gmail.com>
2023-01-13 19:55:11 -0700

    WIP

--- app/services/edamam_service.rb
+++ app/services/edamam_service.rb
@@ -1,3 +1,13 @@
 class EdamamService
 
+  def self.conn
+    Faraday.new("https://api.edamam.com") do |f|
+      f.headers['app_id'] = ENV['edamam_app_id']
+      f.headers['app_key'] = ENV['edamam_api_key']
+  end
+
+  def self.recipes_by_country(country)
+    response = conn.get("/api/recipes/v2?type=public?q=#{country}")
+    JSON.parse(response.body, symbolize_names: true)
+  end
 end
