0c6011ac53323616c67b79dafedd63cf48bebd30
Eli Fuchsman <efuchsman@gmail.com>
2023-01-15 20:36:30 -0700

    Create User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+
+  validates_presence_of :name
+  validates_presence_of :email
+  validates_uniqueness_of :email
+  before_validation :construct_api_key, on: :create
+  validates_presence_of :api_key
+
+  def construct_api_key
+    api_key = Digest::MD5.hexdigest(Time.now.to_s + "#{self.name}#{self.email}")
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_13_215841) do
+ActiveRecord::Schema.define(version: 2023_01_16_030943) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "api_key"
   end
 
 end
